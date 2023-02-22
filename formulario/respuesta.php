feabdf55a2b58a673bb76aa7c850645208172b8b
pc_ek <19307048@utfv.edu.mx>
2023-02-22 16:55:07 -0600

    avance 22

--- formulario/respuesta.php
+++ formulario/respuesta.php
@@ -5,10 +5,34 @@ header ("content-Type: text/html; cherset=UTF-8");
 $usuario = isset($_POST ['usuario']) ? $_POST ['usuario']: '';
 $password = isset($_POST ['password']) ? $_POST ['password']: '';
 
+$usuario2 = '';
+$password2 = '';
 
 $con= new SQLite3('data.db');
 
-$cs = $con -> query ("INSERT INTO login (usuario,password) VALUES ('$usuario','$password')");
+$cs = $con -> query ("SELECT * FROM login WHERE usuario='$usuario' and password='$password'");
+
+while ($result=$cs -> fetchArray()) {
+    $usuario2=$result['usuario'];
+    $password2=$result['password'];
+
+}
+
+if ($usuario == $usuario2) {
+
+    if ($password == $password2) {
+        echo 'usuario y contraseña correcta';
+    }else{
+        echo 'Usuario o contraseña incorrecta';
+    }
+
+
+}else{
+    echo 'Usuario o contraseña incorrecta';
+}
+echo "<br>";
+echo "<br>";
+
 echo $usuario;
 echo "<br>";
 echo $password;
