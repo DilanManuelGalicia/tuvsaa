761b4d5a80d2e2b0e13baf834a84c927745c5455
house_pc <19307048@utfv.edu.mx>
2023-02-25 21:35:56 -0600

    avance 25

--- formulario/index.php
+++ formulario/index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <title>Document</title>
+    <title>Formulario</title>
 </head>
 <body>
     <div class="container-fluid">
@@ -21,8 +21,8 @@
                                 <div class="mb-3">
                                   <label class="form-label">Password</label>
                                   <input type="password" class="form-control" name="password">
+                                  <button type="submit" class="btn btn-primary">Enviar</button>
                                 </div>
-                                <button type="submit" class="btn btn-primary">Enviar</button>
                     </form>
                 
                 </div>       
