423820ab1afd30036bafb2a23f19535183bb655b
pc2k <19307048@utfv.edu.mx>
2023-03-07 17:24:45 -0600

    avance2 07/03

--- respuesta.php
+++ respuesta.php
@@ -4,8 +4,8 @@ header ("content-Type: text/html; cherset=UTF-8");
 
 $correo = isset($_POST ['correo']) ? $_POST ['correo']: '';
 $password = isset($_POST ['password']) ? $_POST ['password']: '';
-$correo2 = '';
-$password2 = '';
+$correo2 = 'ñ';
+$password2 = 'ñ';
 
 $con= new SQLite3('base.db');
 
@@ -16,20 +16,21 @@ while ($result=$cs -> fetchArray()){
     $password2=$result['password'];
 }
 
-/* /* print_r($result);
+/* print_r($result);
 echo '<br>';
 echo $correo2;
 echo $password2; */
-/* */ 
+
 if ($correo == $correo2) {
     
     if ($password == $password2) {
         echo '<script> window.location=("index.html")</script>';
     }else{
-        echo '<script>alert("Correo o Contraseña incorrecta")</script>';
+        echo '<script>alert("Correo o contraseña incorrecto")</script>';
         echo '<script> window.location=("login.html")</script>';
     }
 }else{
-    echo '<script>alert("Correo o Contraseña incorrecta")</script>';
+    echo '<script>alert("Correo o contraseña incorrecto")</script>';
         echo '<script> window.location=("login.html")</script>';
-} */
+} 
+?>
